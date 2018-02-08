3aeaaa64493c4001214d881183fa0e2e0b0be53c
Johnny Reina <johnny.r.reina@gmail.com>
2018-02-07 22:30:17 -0600

    Prettier switched to single quotes i guess

--- test/reporter.spec.js
+++ test/reporter.spec.js
@@ -1,11 +1,11 @@
-const { expect } = require("chai");
+const { expect } = require('chai');
 
-const tests = [{ section: "one" }, { section: "two" }, { section: "three" }];
+const tests = [{ section: 'one' }, { section: 'two' }, { section: 'three' }];
 
 tests.forEach(({ section }) => {
   describe(`test number ${section}`, function() {
     this.meta = { section };
-    it("should pass", () => {
+    it('should pass', () => {
       return Promise.resolve({ test: section });
     });
   });
