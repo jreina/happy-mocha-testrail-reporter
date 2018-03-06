c23bb6dd1fd5b06a21c57cb2d95014ef45477a2a
Johnny Reina <jreina@armt.com>
2018-03-06 09:51:29 -0600

    Adapts meta-test

--- test/reporter.spec.js
+++ test/reporter.spec.js
@@ -1,22 +1,21 @@
 const { expect } = require('chai');
 const Mocha = require('mocha');
 const reporter = require('../lib/reporter');
+const path = require('path');
 
 const reporterOptions = {
   suiteName: 'test suite',
   domain: '',
   username: '',
   password: '',
-  projectId: ''
+  projectId: '1'
 };
 
-describe('Advanced TestRail Reporter', function() {
-  it('Should break', function(done) {
+describe('Advanced TestRail Reporter for Mocha', function() {
+  it('Should run tests and have stuff in the reporter', function(done) {
     const mocha = new Mocha({ reporter, reporterOptions });
-    mocha.addFile('test/src/testrail-publisher.test.js');
-    mocha.run().on('test end', (a, b) => {
-      console.log();
-    });
-    done();
+    const testfile = path.join(__dirname, 'src/dummy.test.js');
+    mocha.addFile(testfile);
+    mocha.run(done);
   });
 });
